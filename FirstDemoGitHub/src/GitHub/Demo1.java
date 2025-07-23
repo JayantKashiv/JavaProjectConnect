e4048b7634fbe9188da2bdaa39c45ecfff207c13
JayantKashiv <134571039+JayantKashiv@users.noreply.github.com>
2025-07-23 12:30:20 +0530

    Update Demo1.java
    Now Can Add Three Numbers

--- FirstDemoGitHub/src/GitHub/Demo1.java
+++ FirstDemoGitHub/src/GitHub/Demo1.java
@@ -4,9 +4,12 @@ public class Demo1 {
 	public static void main(String[] args) {
 			    int first = 10;
 			    int second = 20;
+				int third = 30; 
+				
 
 			    // add two numbers
-			    int sum = first + second;
-			    System.out.println(first + " + " + second + " = "  + sum);
+			// add three numbers
+			    int sum = first + second + third;
+			    System.out.println(first + " + " + second " + " + third +" = "  + sum);
 	}
 }
